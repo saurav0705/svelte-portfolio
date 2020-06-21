aff4e7e5ad4d80b6fb2d1ad7d251993f50212a91
saurav0705 <sauravaggarwal@98gmail.com>
2020-06-21 06:24:30 +0530

    started

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+	import Home from './components/Home.svelte';
+	export let name;
+	let data;
+	onMount(()=> getData());
+	const getData = () => {
+		axios.get('https://port-folio-builder.herokuapp.com/profile/5e8f07fbe24c958234f076f5').then(response => data = response.data);
+	}
+
+
+</script>
+
+<main>
+	{#if data !== undefined}
+	<Home data={data}/>
+	{:else}
+	<p>loading</p>
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Home.svelte
@@ -0,0 +1,28 @@
+<script>
+import {onMount} from 'svelte';
+import lozad from 'lozad';
+export let data;
+let loaded='dcnpm';
+let observe;
+onMount(() => {observe=lozad();observe.observe();});
+
+const checkLoad = () => {
+
+}
+
+
+</script>
+
+<div>
+{#if loaded !==undefined}
+{data.name}
+<img class="lozad" src='21.gif' data-src={data.displayImage} id="image" alt="profile" loading="lazy"/>
+{/if}
+</div>
+
+<style>
+img{
+    height: 200px;
+    width:200px;
+}
+</style>
